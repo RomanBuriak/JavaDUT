755f12dd10a4c2c1c3d35520ccbfeae0df1e452c
RomanBuriak <46033015+RomanBuriak@users.noreply.github.com>
2020-10-20 00:25:24 +0300

    оновлення
    скоро дороблю

--- dz_2/src/com/company/Main.java
+++ dz_2/src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ArrayList<String> arrlist = new ArrayList<String>();
+        arrlist.add("Hello");
+        arrlist.add("You");
+        arrlist.remove();
+        System.out.println(arrlist);
     }
 }
+
